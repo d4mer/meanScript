9682c415bd60921cf06c486f78f4fcf4e916e82a
Ed Arellano <d4mer@d4mations.com>
2017-08-25 18:33:17 +0200

    added pretty comments

--- meanScript.sh
+++ meanScript.sh
@@ -14,33 +14,46 @@
 #       and any other local packages defined in package.json
 
 
+#######################################################################################
+# Set Sudo
+#######################################################################################
+ set -ex
+    SUDO=''
+    if [ "$(id -u)" != "0" ]; then
+      SUDO='sudo'
+      echo "This script requires superuser access to install apt packages."
+      echo "You will be prompted for your password by sudo."
+      # clear any previous sudo permission
+      sudo -k
+    fi
+#######################################################################################
 # Update apt
+#######################################################################################
+
+
 echo "Updating apt"
 apt update && apt upgrade
 
 echo "Let's setup the server"
     #curl -sL https://raw.githubusercontent.com/d4mer/easy-idempiere-server/folder_system/idempiere_server_setup.sh | bash -
 
-
+#######################################################################################
 # Check if Git is installed
+#######################################################################################
+
 echo " - Git Check..."
 which -a git 
 if [[ $? != 0 ]] ; then
     #Install Git
     apt install git
-else
-    git update
+#else
+#    git update
 fi
 
-# Get clone of MEAN.io's repo minus the .git folder and copy to current
-echo " - Cloning MEAN.io..."
-rm -rf .meanStackSetupTemp
-git clone https://github.com/linnovate/mean.git .meanStackSetupTemp || exit $?
-rm -rf .meanStackSetupTemp/.git
-mv .meanStackSetupTemp/{.,}* ./
-
-
+#######################################################################################
 # Check if Node is installed
+#######################################################################################
+
 #   Note that npm comes with node now
 echo " - Node & npm Check..."
 which -a node 
@@ -49,21 +62,28 @@ if [[ $? != 0 ]] ; then
     echo "Downloading and running setup scriptfor node 7"
     curl -sL https://deb.nodesource.com/setup_7.x | bash -
     apt install -y nodejs
-else
-    nodejs update
+#else
+#    nodejs update
 fi
 
+#######################################################################################
 # Install Bower
+#######################################################################################
+
 echo " - Bower Check..."
 which -a bower || npm install -g bower
 
-
+#######################################################################################
 # Install Grunt
+#######################################################################################
+
 echo " - Grunt Check..."
 which -a grunt || npm install -g grunt
 
-
+#######################################################################################
 # Check if Heroku toolbelt is installed
+#######################################################################################
+
 echo " - Heroku Toolbelt Check..."
 which -a heroku
 if [[ $? != 0 ]] ; then
@@ -74,12 +94,14 @@ if [[ $? != 0 ]] ; then
 
     # open https://api.heroku.com/login
     # https://api.heroku.com/signup
-else
-    heroku update
+#else
+#    heroku update
 fi
 
-
+#######################################################################################
 # Install MongoDB
+#######################################################################################
+
 echo " - MongoDB Check..."
 which -a mongo 
 if [[ $? != 0 ]] ; then
@@ -90,16 +112,31 @@ if [[ $? != 0 ]] ; then
     echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
     echo "Installing mongodb-org"
     apt install -y mongodb-org
-else
-    mongodb-org update
+#else
+#    mongodb-org update
 fi
 
+#######################################################################################
 # Install node packages
+#######################################################################################
+
 echo " - Installing package.json packages..."
 npm install
 
+#######################################################################################
+# Get clone of MEAN.io's repo minus the .git folder and copy to current
+#######################################################################################
 
+echo " - Cloning MEAN.io..."
+rm -rf .meanStackSetupTemp
+git clone https://github.com/linnovate/mean.git .meanStackSetupTemp || exit $?
+rm -rf .meanStackSetupTemp/.git
+mv .meanStackSetupTemp/{.,}* ./
+
+#######################################################################################
 # Creating some folders for local db storage and startup mongoDB instance
+#######################################################################################
+
 if [[ ! -d /data/db/ ]]; then
     sudo mkdir -p /data/db/
     sudo chown `id -u` /data/db
@@ -112,7 +149,10 @@ echo " - Starting MongoDB in background"
 ps -ef | grep mongod | awk '{print$2}' | xargs kill {} # Kill old mongodb process
 mongod &
 
-
+#######################################################################################
 # Starting up nodejs server on localhost:3000
+#######################################################################################
+
+
 echo " - Starting app using Grunt"
 grunt
