666f8401930e68f3dc68fe7e6815062a6c946933
Ed Arellano <d4mer@d4mations.com>
2017-08-25 14:54:08 +0200

    adjusted mongo version

--- meanScript.sh
+++ meanScript.sh
@@ -86,9 +86,9 @@ which -s mongo
 if [[ $? != 0 ]] ; then
     #Install MongoDB
     echo "Importing Mongo Key"
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
     echo "Adding to Repo"
-    echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+    echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
     echo "Installing mongodb-org"
     apt install -y mongodb-org
 else
